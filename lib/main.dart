d0f41c0bab7e07dc7974bf70ecc4bcd348a6f86d
Sir_Nath <peternathaniel49@gmail.com>
2022-08-25 01:51:47 +0100

    counter app with cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,19 @@
-import 'package:counter_app_cubit/bloc/cubit/counter_cubit.dart';
 import 'package:counter_app_cubit/constants/routes/routes.dart';
-import 'package:counter_app_cubit/views/screens/first_screen.dart';
-import 'package:counter_app_cubit/views/screens/home_screen.dart';
-import 'package:counter_app_cubit/views/screens/second_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 
 class _MyAppState extends State<MyApp> {
-  //final CounterCubit _counterCubit = CounterCubit();
   final AppRouter _appRouter = AppRouter();
 
   @override
